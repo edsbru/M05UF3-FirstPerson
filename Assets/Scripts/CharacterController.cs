24a80359e00a108f0cdf59293683f3d01560e379
ENTI-PC <a@a.com>
2023-02-27 14:01:17 +0100

    Camara y movimiento

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Camera cam;
+    public Vector2 sensitivity;
+    public Vector2 rotationLimit;
+    public Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        View();
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+       
+        transform.position += transform.forward * vertical + transform.right * horizontal;
+    }
+
+    void View()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * sensitivity.x * Time.deltaTime;
+        float vertical = Input.GetAxis("Mouse Y") * sensitivity.x * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + horizontal, transform.eulerAngles.z);
+        cam.transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x + vertical, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+    }
+}
